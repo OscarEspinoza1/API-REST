f86e2df3eca81ce7093c402864c74610ac927eff
OscarEspinoza1 <osespinoza63@gmail.com>
2025-02-07 22:41:06 -0600

    middleware

--- Parcial1/serverexpress/index.js
+++ Parcial1/serverexpress/index.js
@@ -1,22 +1,54 @@
-import express from 'express'
-import cors from 'cors';
-import { config } from 'dotenv';
-import path from 'node:path'
-import process from "node:process";
-
-config({ path: "./config/.env" });
-
+const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use(cors({
-    origin: "http://localhost",
-    methods: ["GET", "POST"]
-}));
+app.use('/',(req, res, next) => {
+    console.log("peticion al server");
+    next();
+},(req, res, next) => {
+    console.log("2da peticion al middleware");
+    next();
+    });
 
-app.get('/inicio', (_req, res) => {
-    res.sendFile(path.resolve('./public/index.html'));
+app.get('/Alumnos', (req, res) => {
+    console.log(req.query);
+    res.sendFile( __dirname + '/public/index.html');
+    });
+
+app.post('/sistemas/:control', (req, res) => {
+    console.log(req.params);
+    res.send('Got a POST request');
 });
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${ process.env.SERVER_PORT }`);
-})
+
+app.patch('/maestros', (req, res) => {
+    console.log(req.body);
+    res.send('Got a PATCH request');
+});
+
+//Middleware incorporado en express
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Got a POST request');
+    }
+);
+
+app.use((req,res,) => {
+    res.status(404);
+    res.send('Error 404');
+    });
+
+app.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.sendFile( __dirname + '/public/index.html');
+    });
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request');
+    });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    });
